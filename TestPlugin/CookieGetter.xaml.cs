587b972908f2fbd06d673b8b917670ae283291c6
SwetyCore <swety2003@outlook.com>
2022-07-31 22:00:58 +0800

    完善Bili卡片Cookie获取

---
+++ TestPlugin/CookieGetter.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace TestPlugin
+{
+    /// <summary>
+    /// CookieGetter.xaml 的交互逻辑
+    /// </summary>
+    public partial class CookieGetter : Window
+    {
+        public CookieGetter(string url)
+        {
+            new WebView2().Dispose();
+            InitializeComponent();
+            wv2.Source = new Uri(url);
+        }
+        public string Cookie { get; set; }
+        private void GetCookieBtn_Click(object sender, RoutedEventArgs e)
+        {
+            GetCookie();
+        }
+
+        public async Task GetCookie()
+        {
+            Cookie = await wv2.ExecuteScriptAsync("document.cookie");
+            Close();
+        }
+    }
+}
--- TestPlugin/Widgets/BiliHelper.xaml.cs
+++ TestPlugin/Widgets/BiliHelper.xaml.cs
@@ -27,6 +27,7 @@ namespace TestPlugin.Widgets
     {
         DispatcherTimer timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 30, 0) };
         VM.BiliHelper vm = new VM.BiliHelper();
+        String Cookie = "";
         public BiliHelper()
         {
             InitializeComponent();
@@ -88,7 +89,6 @@ namespace TestPlugin.Widgets
         /// </summary>
         public async void DataUpdate(bool tip = true)
         {
-            string Cookie = "buvid3=7F674FC0-5CB0-F077-036F-9B4FE62DB24331528infoc; i-wanna-go-back=-1; _uuid=B9716EB5-5D97-10162-F3C4-37ADFC2DCDDD33415infoc; buvid4=C9812B7A-D134-4703-EFBE-2F19EA44E95034121-022071512-fs30e/wXVzF+VextIYyCZUTr1b3QjpsfXV6v6qBJZAT21pGay5ubhg%3D%3D; CURRENT_BLACKGAP=0; rpdid=0zbfAHRcO4|12PaVwEa5|1p|3w1Ocd0a; fingerprint=9681eba18bb19e664673b8b2b13307ce; buvid_fp_plain=undefined; DedeUserID=156785512; DedeUserID__ckMd5=99e73cdaf36589d6; bili_jct=2783e3df88d6884d9ae390d0da8f0b7d; buvid_fp=9681eba18bb19e664673b8b2b13307ce; blackside_state=0; b_ut=5; nostalgia_conf=-1; LIVE_BUVID=AUTO8016580232683158; hit-dyn-v2=1; PVID=1; CURRENT_FNVAL=4048; bsource=share_source_qqchat; theme_style=light; sid=6160dplx; innersign=0; b_lsid=4121B54E_182523D4BBD; b_timer=%7B%22ffp%22%3A%7B%22333.788.fp.risk_7F674FC0%22%3A%221824F4BFF2F%22%2C%22333.999.fp.risk_7F674FC0%22%3A%221824F4E688D%22%2C%22333.1007.fp.risk_7F674FC0%22%3A%22182523D4EF7%22%7D%7D; bp_video_offset_156785512=688878858413277200";
             if (Cookie != null && Cookie != "" && GetMidFromCookie(Cookie) != "")
             {
                 try
@@ -107,6 +107,14 @@ namespace TestPlugin.Widgets
             {
                 MessageBox.Show("哔哩哔哩:无效的Cookie!");
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dl = new CookieGetter("https://passport.bilibili.com/login");
+            dl.ShowDialog();
+            Cookie=dl.Cookie;
+            DataUpdate(true);
         }
     }
 }
